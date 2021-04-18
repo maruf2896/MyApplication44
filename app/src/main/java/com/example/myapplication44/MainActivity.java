7865d5d3313030ccee3f5e06756dc486fe2d39d5
maruf2896 <ahmedmaruf0234@gmail.com>
2021-04-18 20:35:12 +0600

    Image Slider Issue

---
+++ app/src/main/java/com/example/myapplication44/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication44;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.imageview.ShapeableImageView;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int[]images;
+private SliderAdapter adapter;
+private SliderView  sliderView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+sliderView=findViewById(R.id.SliderView);
+images=new int[]{R.drawable.g2,R.drawable.g3,R.drawable.g4,R.drawable.g5};
+adapter=new SliderAdapter(images);
+sliderView.setSliderAdapter(adapter);
+
+
+
+
+
+
+      sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+      sliderView.setIndicatorAnimation(IndicatorAnimationType.DROP);
+        sliderView.startAutoCycle();
+
+
+
+    }
+}
